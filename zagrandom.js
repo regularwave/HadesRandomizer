84889b5193306b057a86c25410b699e0c4385a43
regularwave <regularwave@gmail.com>
2020-10-05 13:50:47 -0400

    Create zagrandom.js

---
+++ zagrandom.js
@@ -0,0 +1,29 @@
+function myFunction() {
+  var redv = 1;
+  var bluev = 2;
+  var totv = 0;
+  var numres = "";
+  var redcb = document.getElementById("redcb");
+  var bluecb = document.getElementById("bluecb");
+  
+if (redcb.checked == true) { totv += redv };
+if (bluecb.checked == true) { totv += bluev };
+  
+  switch(totv) {
+    case 0:
+      numres = "none";
+      break;
+    case 1:
+      numres = "red";
+      break;
+    case 2:
+      numres = "blue";
+      break;
+    case 3:
+      numres = "purple";
+      break;
+  }
+  
+document.getElementById("text").innerHTML = numres;
+document.getElementById("num").innerHTML = totv;
+}
